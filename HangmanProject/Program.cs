14c8003fbd914e99e57dca15ab4e19d519241c44
nosharp <nosharp1@gmail.com>
2021-06-21 15:23:02 +0100

    feat: start the gamestate on application start

--- HangmanProject/Program.cs
+++ HangmanProject/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using HangmanProject.Player;
 
 namespace HangmanProject
 {
     class Program
     {
         static void Main(string[] args)
-        {}
+        {
+            var game = new Game();
+            game.Start();
+
+            Console.WriteLine("Enter the name of the guesser:");
+            string guesserName = Console.ReadLine();
+            game.CreatePlayer(guesserName, PlayerType.Guesser);
+            
+            Console.WriteLine("Enter the name of the Setter:");
+            string setterName = Console.ReadLine();
+            game.CreatePlayer(setterName, PlayerType.Setter);
+            
+            while (true)
+            {
+                if (game.IsComplete) break;
+                game.OnEventLoop();
+            }
+        }
     }
 }
