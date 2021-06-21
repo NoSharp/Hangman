089cc29c9ee8d9fa25d0392030110b96076e2960
nosharp <nosharp1@gmail.com>
2021-06-21 15:22:12 +0100

    feat: add IHangman for different iterations of hangman

---
+++ HangmanProject/Hangman/IHangman.cs
@@ -0,0 +1,22 @@
+﻿namespace HangmanProject.Hangman
+{
+    public interface IHangman
+    {
+        /// <summary>
+        /// Used to increment the hangman state.
+        /// </summary>
+        void IncrementHangman();
+
+        /// <summary>
+        /// Whether the Hangman complete, and the game should
+        /// be ended.
+        /// </summary>
+        /// <returns> Is the hangman complete </returns>
+        bool IsComplete();
+
+        /// <summary>
+        /// Draw the hangman on the screen.
+        /// </summary>
+        void DrawHangman();
+    }
+}
