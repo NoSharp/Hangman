68a723161e94e8e415471c4ded56662963eaf695
nosharp <nosharp1@gmail.com>
2021-06-21 15:21:44 +0100

    feat: add setter player to set the target word

---
+++ HangmanProject/Player/Setter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HangmanProject.Player
+{
+    public class Setter : IPlayer
+    {
+
+        private Game _game;
+        private string _name;
+        private PlayerType _type;
+        
+        public Setter(string name, PlayerType type)
+        {
+            _name = name;
+            _type = type;
+        }
+        
+        public PlayerType GetType()
+        {
+            return _type;
+        }
+        
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void MountGame(Game game)
+        {
+            _game = game;
+
+            Console.WriteLine($"{this._name} Enter your word");
+            string word = Console.ReadLine();
+
+            _game.TargetWord = word;
+        }
+
+        public void OnRoundProgressed()
+        {
+            
+        }
+
+        public void OnWinGame()
+        {
+            Console.WriteLine($"{this._name}: You've won the game!");
+        }
+
+        public void OnLoseGame()
+        {
+            Console.WriteLine($"{this._name}: You've lost the game!");
+        }
+    }
+}
