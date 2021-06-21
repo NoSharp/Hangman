59e921a2ba9aa0caae4bf8236c825d3ba4de7aae
nosharp <nosharp1@gmail.com>
2021-06-21 15:22:19 +0100

    feat: add HangMan

---
+++ HangmanProject/Hangman/Hangman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HangmanProject.Hangman
+{
+    public class Hangman : IHangman
+    {
+        // The amount of 
+        private uint hangmanPosition = 0;
+        
+        public void IncrementHangman()
+        {
+            hangmanPosition++;
+        }
+
+        public bool IsComplete()
+        {
+            return hangmanPosition == 5;
+        }
+
+        public void DrawHangman()
+        {
+            Console.WriteLine($"You have {5-hangmanPosition} chances left");
+        }
+    }
+}
