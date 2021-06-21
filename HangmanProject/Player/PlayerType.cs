dcad019571d48d3750b9197247c1cded844a401a
nosharp <nosharp1@gmail.com>
2021-06-21 15:22:29 +0100

    feat: add PlayerType

---
+++ HangmanProject/Player/PlayerType.cs
@@ -0,0 +1,14 @@
+﻿namespace HangmanProject.Player
+{
+    public enum PlayerType
+    {
+        /// <summary>
+        /// The one who's guessing the word.
+        /// </summary>
+        Guesser,
+        /// <summary>
+        /// The player who's setting the word to guess.
+        /// </summary>
+        Setter
+    }
+}
