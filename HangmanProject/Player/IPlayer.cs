206a2eeaf67578caa2b7ee16f44e5cbbb2069c67
nosharp <nosharp1@gmail.com>
2021-06-21 15:21:30 +0100

    feat: add IPlayer for abstraction between different types of players.

---
+++ HangmanProject/Player/IPlayer.cs
@@ -0,0 +1,34 @@
+﻿namespace HangmanProject.Player
+{
+    public interface IPlayer
+    {
+
+        PlayerType GetType();
+        
+        /// <summary>
+        /// Used to access the name of the player.
+        /// </summary>
+        string GetName();
+        
+        /// <summary>
+        /// Called when the player is created.
+        /// </summary>
+        /// <param name="game">The Current game state.</param>
+        void MountGame(Game game);
+
+        /// <summary>
+        /// Called when the game progresses one round.
+        /// </summary>
+        void OnRoundProgressed();
+
+        /// <summary>
+        /// Called when the player wins the game.
+        /// </summary>
+        void OnWinGame();
+
+        /// <summary>
+        /// Called when the player has lost the game.
+        /// </summary>
+        void OnLoseGame();
+    }
+}
